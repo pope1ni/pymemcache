a45ba6334e4e738cc562a5471f945b5294148109
Nick Pope <nick.pope@flightdataservices.com>
2020-09-04 12:32:26 +0100

    Tweaked noqa comment to only ignore F401.
    Ensure that only "F401 '...' imported but unused" is ignored.

--- pymemcache/client/__init__.py
+++ pymemcache/client/__init__.py
@@ -1,13 +1,13 @@
 # API Backwards compatibility
 
-from pymemcache.client.base import Client  # noqa
-from pymemcache.client.base import PooledClient  # noqa
-from pymemcache.client.hash import HashClient  # noqa
+from pymemcache.client.base import Client  # noqa: F401
+from pymemcache.client.base import PooledClient  # noqa: F401
+from pymemcache.client.hash import HashClient  # noqa: F401
 
-from pymemcache.exceptions import MemcacheError  # noqa
-from pymemcache.exceptions import MemcacheClientError  # noqa
-from pymemcache.exceptions import MemcacheUnknownCommandError  # noqa
-from pymemcache.exceptions import MemcacheIllegalInputError  # noqa
-from pymemcache.exceptions import MemcacheServerError  # noqa
-from pymemcache.exceptions import MemcacheUnknownError  # noqa
-from pymemcache.exceptions import MemcacheUnexpectedCloseError  # noqa
+from pymemcache.exceptions import MemcacheError  # noqa: F401
+from pymemcache.exceptions import MemcacheClientError  # noqa: F401
+from pymemcache.exceptions import MemcacheUnknownCommandError  # noqa: F401
+from pymemcache.exceptions import MemcacheIllegalInputError  # noqa: F401
+from pymemcache.exceptions import MemcacheServerError  # noqa: F401
+from pymemcache.exceptions import MemcacheUnknownError  # noqa: F401
+from pymemcache.exceptions import MemcacheUnexpectedCloseError  # noqa: F401
