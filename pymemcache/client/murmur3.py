9b060f7c231a8af57b9b00a0e110427c8a1353d9
Nick Pope <nick.pope@flightdataservices.com>
2020-09-04 13:03:57 +0100

    Fixed D208 raised by the flake8-docstrings plugin.
    D208 Docstring is over-indented

--- pymemcache/client/murmur3.py
+++ pymemcache/client/murmur3.py
@@ -1,7 +1,8 @@
 def murmur3_32(data, seed=0):
-    """MurmurHash3 was written by Austin Appleby, and is placed in the
-       public domain. The author hereby disclaims copyright to this source
-       code."""
+    """
+    MurmurHash3 was written by Austin Appleby, and is placed in the public
+    domain. The author hereby disclaims copyright to this source code.
+    """
     c1 = 0xcc9e2d51
     c2 = 0x1b873593
 
